9b5ea41357d1f52c0657cbf57a75980f9036d046
zenxds <zenxds@gmail.com>
2017-12-14 09:49:41 +0800

    update

--- app/web.js
+++ app/web.js
@@ -1,7 +1,16 @@
 const config = require('config')
 const Koa = require('koa')
-const app = new Koa()
+const koaStatic = require('koa-static')
+const session = require('koa-session')
 
+const app = new Koa()
+app.keys = config.get('keys')
+
+app.use(koaStatic(path.join(__dirname, 'public'), {
+  maxage: 0
+}))
+
+app.use(session(app))
 app.use(async ctx => {
   ctx.body = 'Hello World'
 })
--- config/default.js
+++ config/default.js
@@ -1,3 +1,4 @@
 module.exports = {
+  keys: ['58f9014fb686fe9b6449f1769e37ec90a676e9c6'],
   port: 3000
 }
